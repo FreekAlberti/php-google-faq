2b7f3a0ed97989c4d4c5fe3dc384b802e251cee5
FreekAlberti <federicoalberi96@gmail.com>
2020-10-06 15:49:46 +0200

    HTML

--- index.php
+++ index.php
@@ -31,17 +31,6 @@
             "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
         ]
         ];
-
-        foreach ($faq as $problem => $value) {
-            foreach ($value as $text) {
-                if (array_search($text, $value) == "question") {
-                    echo "<strong>".$text."</strong><br>";    
-                } elseif (array_search($text, $value) == "answer") {
-                    echo $text."<br>";
-                }
-            }
-        }
-
 ?>
 
 <!DOCTYPE html>
@@ -52,6 +41,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        foreach ($faq as $problem => $value) {
+            foreach ($value as $text) {
+                if (array_search($text, $value) == "question") { ?>
+                    <h3><?php echo $text; ?></h3>    
+                <?php } elseif (array_search($text, $value) == "answer") { ?>
+                    <h5><?php echo $text; ?></h5>
+                <?php }
+            }
+        } ?>
 </body>
 </html>
