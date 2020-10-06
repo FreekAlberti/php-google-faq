ed3c64a8086e43dcbefe0b3eefe2e7fa8d944e05
FreekAlberti <federicoalberi96@gmail.com>
2020-10-06 17:42:57 +0200

    header

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
             "answer" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
 
             Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
-            
+
             Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
-            
+
             Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
-            
+
             Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
         ],
         [
@@ -17,9 +17,9 @@
             "answer" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
 
             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
-            
+
             Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
-            
+
             Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
         ],
         [
@@ -38,18 +38,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        foreach ($faq as $problem => $value) {
-            foreach ($value as $text) {
-                if (array_search($text, $value) == "question") { ?>
-                    <h3><?php echo $text; ?></h3>    
-                <?php } elseif (array_search($text, $value) == "answer") { ?>
-                    <h5><?php echo $text; ?></h5>
-                <?php }
-            }
-        } ?>
+
+  <header>
+    <div class="container">
+      <img class="logo" src="img/googlelogo.png" alt="logo Google">
+      <div class="header-second-row">
+        <ul class="nav">
+          <li class="elemento">
+            <a class="elm-nav" href="#">Introduzione</a>
+          </li>
+          <li class="elemento">
+            <a class="elm-nav" href="#">Norme sulla privacy</a>
+          </li>
+          <li class="elemento">
+            <a class="elm-nav" href="#">Termini di servizio</a>
+          </li>
+          <li class="elemento">
+            <a class="elm-nav" href="#">Tecnologie</a>
+          </li>
+          <li class="elemento">
+            <a class="elm-nav" href="#">Domande frequenti</a>
+          </li>
+        </ul>
+        <a href="#" class="box-email">
+          <span class="icona-nome">F</span>
+          federicoalberti96@gmail.com
+        </a>
+      </div>
+    </div>
+  </header>
+
+  <section>
+      <div class="wrapper">
+        <?php foreach ($faq as $problem) { ?>
+          <h3 class="domanda"><?php echo $problem["question"]; ?></h3>
+          <h5 class="risposta"><?php echo $problem["answer"]; ?></h5>
+        <? } ?>
+      </div>
+  </section>
+
 </body>
 </html>
